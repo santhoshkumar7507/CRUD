4d4372a1f830d30c7647aa8627d6dd3adc4e5494
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-06-05 16:55:20 +0530

    chore: Rename application to enterprise NexusEdu SIS

--- crud.py
+++ crud.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import os
 
-app = FastAPI()
+app = FastAPI(title="NexusEdu SIS", description="Enterprise Student Information System API", version="1.0.0")
 
 # Mount the static directory
 os.makedirs("static", exist_ok=True)
